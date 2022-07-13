233f2ba2c68bdb61b0de0327806f81fe3b956070
Erick Martinez <analyst_martinez@outlook.com>
2022-07-13 14:06:20 -0400

    Initial commit

---
+++ confirmEnding.js
@@ -0,0 +1,8 @@
+function confirmEnding(str, target) {
+
+    if (str.substring(str.length - target.length) === target) {
+        return true;
+    } else return false;   
+  }
+
+console.log(confirmEnding("Bastian", "n"));
